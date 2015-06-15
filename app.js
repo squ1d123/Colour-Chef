c5384dcfc68b1307cf492d1b8088095ae1e7fbcb
Rutterbrad <rutterbradley@gmail.com>
2015-06-15 18:56:31 +1200

    removed database connection
    made a mistake in thinking that the client should connect to the
    database. Will use restful request

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
 
-var pg = require('pg').native
-  , connectionString = process.env.DATABASE_URL
-  , client
-  , query;
-
-client = new pg.Client(connectionString);
-client.connect();
-
-
-
-
 
 /*
 	trys to enter the user into the database.
 */
 function create_new_user (name,age,difficulty) {
 
-	client.connect(function (err) {
-		
-		if(err){
-			//there was a connection error
-			return;
-		}
-
-		//connected
-	});
+	
 
 
-
-
-
-	query.on('end', function(result) { client.end(); });
+	
 	return true;// or false
 }
 
