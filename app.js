f9a027b76b36ea7c126c04af82f1bbb8684d9f97
Rutterbrad <rutterbradley@gmail.com>
2015-06-15 18:18:14 +1200

    added client connect and end for new_user method

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 
+var pg = require('pg').native
+  , connectionString = process.env.DATABASE_URL
+  , client
+  , query;
+
+client = new pg.Client(connectionString);
+client.connect();
+
+
 
 
 
@@ -7,10 +16,22 @@
 */
 function create_new_user (name,age,difficulty) {
 
-	
+	client.connect(function (err) {
+		
+		if(err){
+			//there was a connection error
+			return;
+		}
 
-	
-	return true;//false
+		//connected
+	});
+
+
+
+
+
+	query.on('end', function(result) { client.end(); });
+	return true;// or false
 }
 
 
