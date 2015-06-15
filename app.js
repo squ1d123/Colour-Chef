64597c8e26ecd833b76908849a4cc44d5f69dc81
Rutterbrad <rutterbradley@gmail.com>
2015-06-15 21:14:11 +1200

    ready for next function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 	trys to enter the user into the database.
 */
 function create_new_user (url, name, age, difficulty) {
-	var Posturl = url+'/newUser'
+	var Posturl = url+'/newUser';
     var newUser = { "name" : name, "age" : age, "difficulty" : difficulty };
 
     $.post(Posturl,newUser, function(data) {
@@ -25,12 +25,31 @@ function create_new_user (url, name, age, difficulty) {
 	add friend to user
 */
 function add_friend (url, user, friend, friend_code) {
-	// body...
+	var Posturl = url+'/addFriend';
+
+	var friendRequest = {"user" : user, "friend" : friend, "code" : friend_code};
+
+	$.post(Posturl,friendRequest, function(data) {
+
+      if(data.error){
+        //error in request or in database insert
+
+        //false
+      }
+
+
+      //true
+    }, 'json');
+
 }
 
 
-
-
+/*
+	
+*/
+function function_name (argument) {
+	// body...
+}
 
 
 
