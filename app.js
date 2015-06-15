eb522b7c1633261db9ab0dad13de92d772aa2b76
Rutterbrad <rutterbradley@gmail.com>
2015-06-15 19:58:11 +1200

    implemented most of the create_new_user
    still need to implement the return parts true and false. will wait to
    see designers code first

--- app.js
+++ app.js
@@ -3,13 +3,21 @@
 /*
 	trys to enter the user into the database.
 */
-function create_new_user (name,age,difficulty) {
+function create_new_user (url,name,age,difficulty) {
+	var Posturl = url+'/newUser'
+    var newUser = { "name" : name, "age" : age, "difficulty" : difficulty };
 
-	
+    $.post(Posturl,newUser, function(data) {
+
+      if(data.error){
+        //error in request or in database insert
+
+        //false
+      }
 
 
-	
-	return true;// or false
+      //true
+    }, 'json');
 }
 
 
